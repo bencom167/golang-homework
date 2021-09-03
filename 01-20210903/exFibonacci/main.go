57951150816e6c12cb6a110b430177a683a785eb
Ben Ngoc Dang <bencom167@gmail.com>
2021-09-03 22:27:51 +0700

    Update bai tinh Fibonacci

--- 01-20210903/exFibonacci/main.go
+++ 01-20210903/exFibonacci/main.go
@@ -2,32 +2,76 @@ package main
 
 import (
 	// Thu vien chuan
-
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-
 	// Khai bao bien
+	var value float64
 	var (
-		firstNum  float64
-		secondNum float64
+		err    error
+		target float64
 	)
 
-	firstNum = 1
-	secondNum = 1
+	target, err = readNumberFromKeyboard("Nhap so Fibonacci thu : ")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	target := 10
+	// Su dung loop de tinh
+	// value = methodUsingLoop(int(target))
 
-	number := returnNextNumberFibonacci(firstNum, secondNum)
+	// Su dung De qui de tinh
+	value = methodUsingRecursion(int(target))
 
-	fmt.Print("So Fibonacci thu ", target, " : ", number)
+	fmt.Println("So Fibonacci thu ", target, " co gia tri : ", value)
 }
 
-/*
-Hàm đọc từ bàn phím
-*/
-func returnNextNumberFibonacci(a float64, b float64) (result float64) {
-	result = a + b
-	return result
+// Ham su dung loop For thong thuong
+func methodUsingLoop(target int) float64 {
+	var previousNum = 1.0
+	var currentNum = 1.0
+
+	// Voi thu tu so la 1,2 thi tra lai gia tri luon
+	if target < 3 {
+		return 1.0
+	}
+
+	// Tu 3 tro len thi lap tinh
+	for i := 2; i < target; i++ {
+		nextNum := previousNum + currentNum
+		previousNum = currentNum
+		currentNum = nextNum
+	}
+
+	return currentNum
+}
+
+// Ham De quy
+func methodUsingRecursion(target int) float64 {
+	if target <= 2 {
+		return 1
+	}
+	// Goi de quy tinh cac so thu tu truoc do
+	return methodUsingRecursion(target-1) + methodUsingRecursion(target-2)
+}
+
+// Ham do so tu ban phim
+func readNumberFromKeyboard(msg string) (result float64, err error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(msg)
+	str, _ := reader.ReadString('\n')
+	str = strings.TrimSuffix(str, "\n")
+
+	if result, err = strconv.ParseFloat(str, 64); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	return result, nil
 }
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+/*
+Lệnh dùng để push code lên git
+*/
+
+git add -A
+git commit -m 'Description here'
+git push -u -f origin master
